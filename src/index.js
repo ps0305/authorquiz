f2214d88a599b89908f522b46b0ca121fd326f25
ps0305 <singh.pankaj@live.com>
2018-09-14 14:20:12 +0530

    image path

--- src/index.js
+++ src/index.js
@@ -9,39 +9,39 @@ import {shuffle,sample} from 'underscore'
 const authors = [
     {
       name: 'Mark Twain',
-      imageUrl: 'images/authors/marktwain.jpg',
+      imageUrl: '../authors/marktwain.jpg',
       imageSource: 'Wikimedia Commons',
       books: ['The Adventures of Huckleberry Finn']
     },
     {
       name: 'Joseph Conrad',
-      imageUrl: 'images/authors/josephconrad.png',
+      imageUrl: '../authors/josephconrad.png',
       imageSource: 'Wikimedia Commons',
       books: ['Heart of Darkness']
     },
     {
       name: 'J.K. Rowling',
-      imageUrl: 'images/authors/jkrowling.jpg',
+      imageUrl: '../authors/jkrowling.jpg',
       imageSource: 'Wikimedia Commons',
       imageAttribution: 'Daniel Ogren',
       books: ['Harry Potter and the Sorcerers Stone']
     },
     {
       name: 'Stephen King',
-      imageUrl: 'images/authors/stephenking.jpg',
+      imageUrl: '../authors/stephenking.jpg',
       imageSource: 'Wikimedia Commons',
       imageAttribution: 'Pinguino',
       books: ['The Shining', 'IT']
     },
     {
       name: 'Charles Dickens',
-      imageUrl: 'images/authors/charlesdickens.jpg',
+      imageUrl: '../authors/charlesdickens.jpg',
       imageSource: 'Wikimedia Commons',
       books: ['David Copperfield', 'A Tale of Two Cities']
     },
     {
       name: 'William Shakespeare',
-      imageUrl: 'images/authors/williamshakespeare.jpg',
+      imageUrl: '../authors/williamshakespeare.jpg',
       imageSource: 'Wikimedia Commons',
       books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
     }
