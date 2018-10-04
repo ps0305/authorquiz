b32f02bbf40e30a2f8fe1bf084e04812236597b5
ps0305 <singh.pankaj@live.com>
2018-10-04 15:04:15 +0530

    redux

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,7 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types"
-import "./App.css";
-import "./bootstrap.min.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import './App.css';
+import './bootstrap.min.css';
 
 
 function Hero() {
@@ -11,6 +13,13 @@ function Hero() {
         <h1>Author Quiz</h1>
         <p>Select the book written by Author shown</p>
       </div>
+    </div>
+  );
+}
+function Book({ title, onClick }) {
+  return (
+    <div className="answer" onClick={()=>{onClick(title);}}>
+      <h4>{title}</h4>
     </div>
   );
 }
@@ -45,47 +54,59 @@ Turn.propTypes = {
   books: PropTypes.arrayOf(PropTypes.string).isRequired,
   onAnswerSelected: PropTypes.func.isRequired,
   highlight: PropTypes.string.isRequired
-}
+};
 
-
-function Book({ title, onClick }) {
+function Continue({ show, onContinue }) {
   return (
-    <div className="answer" onClick={()=>{onClick(title);}}>
-      <h4>{title}</h4>
+    <div className="row continue">
+    { show 
+      ? <div className="col-11">
+          <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button>
+        </div>
+      : null }
     </div>
   );
 }
-function Continue() {
-  return <div />;
-}
+
 function Footer() {
-  return (
-    <div id="footer" className="row">
-      <div className="col-12">
+  return (<div id="footer" className="row">
+    <div className="col-12">
         <p className="text-muted credit">
-          All images are from{" "}
-          <a href="http://commons.wikimedia.org/wiki/Main_Page">
-            Wikemedia Commons
-          </a>{" "}
-          and are in the public domain
+          All images are from <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a> and are in the public domain
         </p>
-      </div>
     </div>
-  );
+  </div>);
 }
-//adding highlight props
-//AuthorQuiz will provide onAnswerSelected as a props to Turn fucntion
-function AuthorQuiz({ turnData,highlight, onAnswerSelected }) {
-  return (
-    <div>
+
+function mapStateToProps(state) {
+  return {
+    turnData: state.turnData,
+    highlight: state.highlight
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onAnswerSelected: (answer) => {
+      dispatch({ type: 'ANSWER_SELECTED', answer });
+    },
+    onContinue: () => {
+      dispatch({ type: 'CONTINUE' });
+    }
+  };
+}
+
+const AuthorQuiz = connect(mapStateToProps, mapDispatchToProps)(
+  function ({turnData, highlight, onAnswerSelected, onContinue}) {
+    return (
       <div className="container-fluid">
         <Hero />
-        <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
-        <Continue />
+        <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+        <Continue show={highlight === 'correct'} onContinue={onContinue}/>
+        <p><Link to="/add">Add an author</Link></p>
         <Footer />
       </div>
-    </div>
-  );
-}
+    );
+  });
 
 export default AuthorQuiz;
